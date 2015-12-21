579038b8ee1f428fd93ba33288dc7d55b075ce88
Alpay Önal <alpaycom@gmail.com>
2015-12-21 14:10:43 +0200

    Update test.py
    dasdasd

--- test.py
+++ test.py
@@ -1,28 +1,6 @@
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdasadsa das
- dasd
-  as
-  d
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
-aasdas dsa da sdsa das dsdasdasasdadadadaasdasd asd
-asd
+asdsadasdas
+das
+d
+sa
+d
 as
-ddas da d
-a
-adsdasdasas das das
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
-adsdasdas
